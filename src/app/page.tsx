f1ba46bc70a637ec05d43d589d3cd47af8cfd76d
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-08-14 18:46:27 +0300

    Configuring the container and making media rules fonts-colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,24 @@ import Socials from '@/components/ui/socials';
 const Home = () => {
   return (
     <main>
-      <Logo />
-
-      <br />
-
-      <button className="custom-button custom-button-no-border">
-        Записатися
-      </button>
-
-      <button className="custom-button custom-button-border">Записатися</button>
-
-      <div className="py-4">
-        <Socials color="text-greenDarkText" hoverColor="hover:text-blue" />
-      </div>
-
-      <div className="bg-blue py-4">
-        <Socials />
+      <div className="container">
+        <Logo />
+    
+        <br />
+    
+        <button className="custom-button custom-button-no-border">
+          Записатися
+        </button>
+    
+        <button className="custom-button custom-button-border">Записатися</button>
+    
+        <div className="py-4">
+          <Socials color="text-greenDarkText" hoverColor="hover:text-blue" />
+        </div>
+    
+        <div className="bg-blue py-4">
+          <Socials />
+        </div>
       </div>
     </main>
   );
