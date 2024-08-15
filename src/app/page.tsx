d9fc998d67782f822d80b8a4a840fef3369d2d8e
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-08-15 10:56:12 +0300

    added container in page tsx  fonts-colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,26 @@ import Socials from '@/components/ui/socials/socials';
 const Home = () => {
   return (
     <main>
-      <div className='container'>
-      <Logo />
+      <div className="container">
+        <Logo />
 
-      <br />
+        <br />
 
-      <button className="custom-button custom-button-no-border">
-        Записатися
-      </button>
+        <button className="custom-button custom-button-no-border">
+          Записатися
+        </button>
 
-      <button className="custom-button custom-button-border">Записатися</button>
+        <button className="custom-button custom-button-border">
+          Записатися
+        </button>
 
-      <div className="py-4">
-        <Socials section="menu" />
-      </div>
+        <div className="py-4">
+          <Socials section="menu" />
+        </div>
 
-      <div className="bg-blue py-4">
-        <Socials section="footer" />
-      </div>
+        <div className="bg-blue py-4">
+          <Socials section="footer" />
+        </div>
       </div>
     </main>
   );
