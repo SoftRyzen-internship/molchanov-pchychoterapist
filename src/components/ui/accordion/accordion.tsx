267d8f7323ee989fff908a5279fb04462d4a11bc
Svitlana Zhovanik <lanasvetcat@gmail.com>
2024-08-20 15:41:21 +0300

    fix accordion

--- src/components/ui/accordion/accordion.tsx
+++ src/components/ui/accordion/accordion.tsx
@@ -9,46 +9,62 @@ import faqs from '@/data/FAQ/FAQ.json';
 import clsx from 'clsx';
 
 export const Accordion: React.FC = () => {
-  const [openIndex, setOpenIndex] = useState<number | null>(0);
-  console.log(openIndex);
+  const [activeDisclosurePanel, setActiveDisclosurePanel] = useState<{
+    key: number;
+    open: boolean;
+    close?: () => void;
+  } | null>(null);
+  function togglePanels(newPanel: { key: number; open: boolean }) {
+    if (activeDisclosurePanel && activeDisclosurePanel.close) {
+      if (
+        activeDisclosurePanel.key !== newPanel.key &&
+        activeDisclosurePanel.open
+      ) {
+        activeDisclosurePanel.close();
+      }
+    }
+    setActiveDisclosurePanel({
+      ...newPanel,
+      open: !newPanel.open,
+    });
+  }
   return (
     <ul className="min-w-[320px]">
       {faqs.accordion.map((faq, index) => (
         <Disclosure
           key={faq.id}
           as="li"
-          className={clsx('border-b border-greenDarkText', {
-            'py-4': openIndex !== index,
-            'pt-4': openIndex === index,
-            'md:border-b-0': index === faqs.accordion.length - 1,
+          className={clsx('border-b border-greenDarkText py-4', {
+            'md:border-b-0': faqs.accordion.length - 1 === index,
           })}
-          defaultOpen={openIndex === index}
         >
-          {({ open }) => (
-            <>
-              <DisclosureButton
-                className="flex justify-between gap-8 w-full  "
-                onClick={() => setOpenIndex(open ? -1 : index)}
-              >
-                <p className="text-left ">{faq.question}</p>
-                <Arrow
-                  className={clsx(
-                    'min-w-4 min-h-4 transform transition-transform duration-300',
-                    {
-                      'rotate-180': openIndex === index,
-                      'w-4 h-4 ': true,
-                      'md:min-w-8 md:min-h-8': true,
-                    }
-                  )}
-                />
-              </DisclosureButton>
-              {openIndex === index && (
+          {(panel) => {
+            const { open } = panel;
+            return (
+              <>
+                <DisclosureButton
+                  className="flex justify-between gap-8 w-full"
+                  onClick={() => togglePanels({ ...panel, key: index })}
+                >
+                  <p className="text-left">{faq.question}</p>
+                  <Arrow
+                    className={clsx(
+                      'min-w-4 min-h-4 transform transition-transform duration-300',
+                      {
+                        'rotate-180': open,
+                        'rotate-0': !open,
+                        'w-4 h-4 ': true,
+                        'md:min-w-8 md:min-h-8': true,
+                      }
+                    )}
+                  />
+                </DisclosureButton>
                 <DisclosurePanel className="text-[14px] font-light my-3 md:text-[16px] md:font-medium">
                   {faq.answers}
                 </DisclosurePanel>
-              )}
-            </>
-          )}
+              </>
+            );
+          }}
         </Disclosure>
       ))}
     </ul>
