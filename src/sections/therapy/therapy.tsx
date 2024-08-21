e11e1ac499705452be9c042fbfe194a2a5f37374
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-08-21 16:42:41 +0300

    therapy on desktop section-therapy

--- src/sections/therapy/therapy.tsx
+++ src/sections/therapy/therapy.tsx
@@ -5,13 +5,15 @@ import therapyData from '@/data/therapy.json';
 export const Therapy = () => {
   return (
     <section className="py-[45px] md:py-[50px] xl:pt-[77px] xl:pb-[106px]">
-      <h2 className="mb-10 mdOnly:w-[505px] md:mb-[60px] xl:text-center">{therapy.title}</h2>
+      <h2 className="mb-10 mdOnly:w-[505px] md:mb-[60px] xl:text-center">
+        {therapy.title}
+      </h2>
 
-        <ul className="hidden xl:flex xl:flex-row justify-between">
-          {therapyData.therapySteps.map((item) => (
-            <TherapyItem key={item.id} item={item} />
-          ))}
-        </ul>
+      <ul className="hidden xl:flex xl:flex-row justify-between">
+        {therapyData.therapySteps.map((item) => (
+          <TherapyItem key={item.id} item={item} />
+        ))}
+      </ul>
     </section>
   );
 };
