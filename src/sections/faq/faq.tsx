9712d1252941451ed7929ec2e63774db96087de6
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-08-22 12:27:58 +0300

    correct header indents

--- src/sections/faq/faq.tsx
+++ src/sections/faq/faq.tsx
@@ -4,9 +4,9 @@ import { Accordion } from '@/components/common/accordion/accordion';
 
 export const FAQ = () => {
   return (
-    <section className="bg-pinkSupperLight py-[40px] md:py-[60px] xl:py-[70px] ">
+    <section className="bg-pinkSupperLight py-10 md:py-[60px] xl:py-[70px] ">
       <div className="container">
-        <h2 className="mb-10 md:mb-[60px] xl:text-center">{faq.title}</h2>
+        <h2 className="mb-6 md:mb-11 xl:text-center">{faq.title}</h2>
         <Accordion />
       </div>
     </section>
