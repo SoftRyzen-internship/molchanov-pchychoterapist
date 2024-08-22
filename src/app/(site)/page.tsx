a9ff2068dc99d498c4785f2afdd9445e30a52a8f
Marynryn <murmar95@gmail.com>
2024-08-22 15:27:52 +0300

    create header

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -32,6 +32,7 @@ import { PortableText } from '@portabletext/react';
 
 import { ContactForm } from '@/components/common/contact-form';
 import { Services } from '@/sections/services/services';
+import { Header } from '@/layout/Header/header';
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -50,19 +51,66 @@ const Home = () => {
   }, []);
 
   return (
-    <main>
-      <Services />
-      <TargetAudience />
-      <div className="container">
-        <ul className=" xl:hidden">
-          <Slider>
-            {therapyData.therapySteps.map((item) => (
-              <TherapyItem key={item.id} item={item} />
-            ))}
-          </Slider>
-        </ul>
-        <ul>
-          <Slider>
+    <>
+      <Header />
+      <main>
+        <Services />
+        <TargetAudience />
+        <div className="container">
+          <ul className=" xl:hidden">
+            <Slider>
+              {therapyData.therapySteps.map((item) => (
+                <TherapyItem key={item.id} item={item} />
+              ))}
+            </Slider>
+          </ul>
+          <ul>
+            <Slider>
+              {reviews.reviews.map((review) => (
+                <ReviewCard
+                  key={review.id}
+                  author={review.author}
+                  text={review.text}
+                />
+              ))}
+            </Slider>
+          </ul>
+
+          <Accordion />
+          <Logo />
+
+          <br />
+
+          <button className="custom-button custom-button-no-border">
+            Записатися
+          </button>
+
+          <button className="custom-button custom-button-border">
+            Записатися
+          </button>
+
+          <div className="py-4">
+            <Socials section="menu" />
+          </div>
+
+          <div className="bg-blue py-4">
+            <Socials section="footer" />
+          </div>
+          <ul>
+            <ValuesCard
+              id={cardsValuesData.cards[0].id}
+              name={cardsValuesData.cards[2].name}
+              title={cardsValuesData.cards[0].title}
+              text={cardsValuesData.cards[0].text}
+            />
+          </ul>
+          <NavMenu section="burger" />
+          <NavMenu section="header" />
+          <div className="bg-blue ">
+            <NavMenu section="footer" />
+          </div>
+
+          <ul>
             {reviews.reviews.map((review) => (
               <ReviewCard
                 key={review.id}
@@ -70,123 +118,79 @@ const Home = () => {
                 text={review.text}
               />
             ))}
-          </Slider>
-        </ul>
+          </ul>
 
-        <Accordion />
-        <Logo />
-
-        <br />
-
-        <button className="custom-button custom-button-no-border">
-          Записатися
-        </button>
-
-        <button className="custom-button custom-button-border">
-          Записатися
-        </button>
-
-        <div className="py-4">
-          <Socials section="menu" />
-        </div>
-
-        <div className="bg-blue py-4">
-          <Socials section="footer" />
-        </div>
-        <ul>
-          <ValuesCard
-            id={cardsValuesData.cards[0].id}
-            name={cardsValuesData.cards[2].name}
-            title={cardsValuesData.cards[0].title}
-            text={cardsValuesData.cards[0].text}
-          />
-        </ul>
-        <NavMenu section="burger" />
-        <NavMenu section="header" />
-        <div className="bg-blue ">
-          <NavMenu section="footer" />
-        </div>
-
-        <ul>
-          {reviews.reviews.map((review) => (
-            <ReviewCard
-              key={review.id}
-              author={review.author}
-              text={review.text}
-            />
-          ))}
-        </ul>
-
-        <ContactForm />
-
-        <div>
-          <h2>Title h2</h2>
-          <h3>Title h3</h3>
-          <p>MainText</p>
-        </div>
-
-        <ul className="flex flex-col gap-6 md:flex-row md:flex-wrap md:justify-between md:gap-x-0 md:gap-y-7 xl:gap-y-8 xl:w-[610px]">
-          {aboutData.awards.map((award) => (
-            <AwardItem key={award.id} item={award} />
-          ))}
-        </ul>
-
-        <ul className="pr-[25px] md:pr-0 xl:flex xl:flex-row justify-between">
-          {therapyData.therapySteps.map((item) => (
-            <TherapyItem key={item.id} item={item} />
-          ))}
-        </ul>
-
-        <div className="py-10">
-          <h2 className="h2 mb-10">Оберіть, як відправилась форма</h2>
-
-          <div className="mb-10">
-            <label className="mr-2">
-              <input
-                type="radio"
-                name="status"
-                value="success"
-                checked={status === 'success'}
-                onChange={() => setStatus('success')}
-                className="mr-1"
-              />
-              Success
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="status"
-                value="failed"
-                checked={status === 'failed'}
-                onChange={() => setStatus('failed')}
-                className="mr-1"
-              />
-              Failed
-            </label>
-          </div>
+          <ContactForm />
 
           <div>
-            <button
-              onClick={() => setIsOpen(true)}
-              className="custom-button custom-button-no-border"
-            >
-              Показати повідомлення
-            </button>
-            <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-              <FormFeedbackMessage status={status} />
-            </Modal>
+            <h2>Title h2</h2>
+            <h3>Title h3</h3>
+            <p>MainText</p>
           </div>
-        </div>
 
-        <ul>
-          {politics.map((section, index) => (
-            <li key={index}>
-              <PortableText value={section} />
-            </li>
-          ))}
-        </ul>
-      </div>
-    </main>
+          <ul className="flex flex-col gap-6 md:flex-row md:flex-wrap md:justify-between md:gap-x-0 md:gap-y-7 xl:gap-y-8 xl:w-[610px]">
+            {aboutData.awards.map((award) => (
+              <AwardItem key={award.id} item={award} />
+            ))}
+          </ul>
+
+          <ul className="pr-[25px] md:pr-0 xl:flex xl:flex-row justify-between">
+            {therapyData.therapySteps.map((item) => (
+              <TherapyItem key={item.id} item={item} />
+            ))}
+          </ul>
+
+          <div className="py-10">
+            <h2 className="h2 mb-10">Оберіть, як відправилась форма</h2>
+
+            <div className="mb-10">
+              <label className="mr-2">
+                <input
+                  type="radio"
+                  name="status"
+                  value="success"
+                  checked={status === 'success'}
+                  onChange={() => setStatus('success')}
+                  className="mr-1"
+                />
+                Success
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="status"
+                  value="failed"
+                  checked={status === 'failed'}
+                  onChange={() => setStatus('failed')}
+                  className="mr-1"
+                />
+                Failed
+              </label>
+            </div>
+
+            <div>
+              <button
+                onClick={() => setIsOpen(true)}
+                className="custom-button custom-button-no-border"
+              >
+                Показати повідомлення
+              </button>
+              <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+                <FormFeedbackMessage status={status} />
+              </Modal>
+            </div>
+          </div>
+
+          <ul>
+            {politics.map((section, index) => (
+              <li key={index}>
+                <PortableText value={section} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
+    </>
   );
 };
 
---
+++ src/layout/Header/header.tsx
@@ -0,0 +1,12 @@
+import { Logo } from '@/components/ui/logo/logo';
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header className="">
+      <div className="container">
+        <Logo />
+      </div>
+    </header>
+  );
+};
