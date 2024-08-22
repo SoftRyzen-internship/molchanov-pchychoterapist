c493736e7c33626971b542158480a6878085af9e
bonny-art <svitlana.otenko@gmail.com>
2024-08-22 18:22:24 +0300

    section-about: Image styled in mob and tabl

--- src/sections/about/about.tsx
+++ src/sections/about/about.tsx
@@ -8,8 +8,8 @@ export const About = () => {
       <div className="container">
         <h2 className="h2">{aboutData.title}</h2>
 
-        <div className="relative mb-[120px]">
-          <div className="relative w-full h-[411px] rounded-lg overflow-hidden">
+        <div className="relative mb-[120px] md:mb-[130px]">
+          <div className="relative w-full md:w-[575px] xl:w-[505px] h-[411px] md:h-[667px] xl:h-[586px] rounded-lg overflow-hidden">
             <Image
               src="/assets/images/webp/about/my-photo.webp"
               alt="Сергій Молчанов"
@@ -19,8 +19,8 @@ export const About = () => {
             />
           </div>
 
-          <div className="absolute -bottom-20 left-0 w-full max-w-[440px] pl-[72px] pr-[21px]">
-            <div className="relative w-full  h-[152px] rounded-lg overflow-hidden">
+          <div className="absolute -bottom-20 left-0 w-full smOnly:max-w-[440px] pl-[72px] md:pl-[367px] pr-[21px] md:pr-0">
+            <div className="relative w-full md:w-[339px] md:h-[252px] h-[152px] rounded-lg overflow-hidden">
               <Image
                 src="/assets/images/webp/about/therapy-meeting.webp"
                 alt="Сеанс терапії Сергія Молчанова з групою клієнтів"
