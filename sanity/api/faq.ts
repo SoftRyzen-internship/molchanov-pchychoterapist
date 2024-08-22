41a8a5e22eb93cb010f519ea0ecb745a41f3609f
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-08-22 19:01:37 +0300

    fixed-accordion

--- sanity/api/faq.ts
+++ sanity/api/faq.ts
@@ -1,12 +1,6 @@
 import { client } from '../lib/client';
 
-interface FaqItem {
-  _key: string;
-  question: string;
-  answer: string;
-}
-
-export async function getFaq(): Promise<FaqItem[]> {
+export async function getFaq() {
   const query = '*[_type == "faq"]{faqList[]{_key, question, answer}}';
   const faq = await client.fetch(query);
   return faq[0].faqList;
