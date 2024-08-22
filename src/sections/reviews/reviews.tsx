a546eb6889adc9709557972f8f0cf3491f07b496
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-08-22 14:47:16 +0300

    layout all section reviews

--- src/sections/reviews/reviews.tsx
+++ src/sections/reviews/reviews.tsx
@@ -9,6 +9,13 @@ export const Reviews = () => {
       <div className="container">
         <div className="flex flex-col xl:flex-row xl:items-center xl:justify-between">
           <div className="xl:w-1/2">
+          <Image
+              src={reviews.quotes.path}
+              width={32}
+              height={30}
+              alt={reviews.quotes.alt}
+              className="mb-5 md:w-[70px] md:mb-8"
+            />
             <Slider>
               {reviews.reviews.map((review) => (
                 <ReviewCard
@@ -19,13 +26,14 @@ export const Reviews = () => {
               ))}
             </Slider>
           </div>
+
           <div className="xl:w-1/2">
             <Image
               src={reviews.image.path}
               width={254}
               height={237}
               alt={reviews.image.alt}
-              className="mx-auto mt-4 md:w-[633px] md:mt-[60px] xl:w-[561px]"
+              className="smOnly:mx-auto smOnly:mt-4 md:w-[633px] mdOnly:mx-auto mdOnly:mt-[60px] xl:ml-auto xl:w-[561px] "
             />
           </div>
         </div>
