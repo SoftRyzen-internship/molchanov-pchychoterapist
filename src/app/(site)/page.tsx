677ad962d743c6d43133fc8873623e64e9ef76a5
Marynryn <murmar95@gmail.com>
2024-08-22 16:03:26 +0300

    style for header

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { Services } from '@/sections/services/services';
-
 import { TargetAudience } from '@/sections/target-audience/target-audience';
 import { Therapy } from '@/sections/therapy/therapy';
 import { Values } from '@/sections/values/values';
--- src/layout/Header/header.tsx
+++ src/layout/Header/header.tsx
@@ -1,11 +1,26 @@
+'use client';
+
 import { Logo } from '@/components/ui/logo/logo';
+import { NavMenu } from '@/components/ui/nav-menu/nav-menu';
+import { useIsDesktop } from '@/hooks/use-is-desctop';
 import React from 'react';
+import content from '@/data/header.json';
 
 export const Header = () => {
+  const isDesktop = useIsDesktop();
   return (
     <header className="">
       <div className="container">
         <Logo />
+        {!isDesktop ? (
+          <button type="button" className="">
+            {/* <Burger/> */}
+          </button>
+        ) : (
+          <>
+            <NavMenu /> <button type="button">{content.button}</button>
+          </>
+        )}
       </div>
     </header>
   );
