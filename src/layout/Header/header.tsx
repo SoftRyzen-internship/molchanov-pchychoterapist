ecacc4130fb89b0e4961a29ff376f8d805656d06
Marynryn <murmar95@gmail.com>
2024-08-22 21:28:00 +0300

    iaDEsktop

--- src/layout/Header/header.tsx
+++ src/layout/Header/header.tsx
@@ -2,18 +2,19 @@
 
 import { Logo } from '@/components/ui/logo/logo';
 import { NavMenu } from '@/components/ui/nav-menu/nav-menu';
-import { useIsDesktop } from '@/hooks/use-is-desctop';
+
 import { Link as ScrollLink } from 'react-scroll';
 
 import React from 'react';
 import content from '@/data/header.json';
+import { useDeviceType } from '@/hooks/use-device-type';
 
 export const Header = () => {
-  const isDesktop = useIsDesktop();
+  const { isDesktop } = useDeviceType();
   console.log(isDesktop);
   return (
     <header className="">
-      <div className="container">
+      <div className="container flex justify-between">
         <Logo />
         {!isDesktop ? (
           <button type="button" className="">
@@ -21,7 +22,7 @@ export const Header = () => {
           </button>
         ) : (
           <>
-            <NavMenu />
+            <NavMenu section="header" />
             <ScrollLink
               className="custom-button custom-button-border "
               to={content.href}
