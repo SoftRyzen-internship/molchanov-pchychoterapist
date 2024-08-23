2690e1a282f27b28a77d52c191297ece9739a056
Marynryn <murmar95@gmail.com>
2024-08-23 11:29:50 +0300

    id for ssections

--- src/sections/services/services.tsx
+++ src/sections/services/services.tsx
@@ -3,7 +3,7 @@ import servicesData from '@/data/services.json';
 
 export const Services = () => {
   return (
-    <section className="services py-[45px] md:py-[50px] xl:py-[75px]">
+    <section id="services" className=" py-[45px] md:py-[50px] xl:py-[75px]">
       <div className="container">
         <h2 className="h2 mb-8 md:mb-[60px]">{servicesData.title}</h2>
 
