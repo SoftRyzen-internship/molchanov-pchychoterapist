0393625834b817a99d6fba9dabf05888c0da6502
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-08-23 11:43:10 +0300

    fixed-faq-section

--- src/sections/faq/faq.tsx
+++ src/sections/faq/faq.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import faq from '@/data/faq.json';
 import { Accordion } from '@/components/common/accordion/accordion';
+import { getFaq } from '../../../sanity/api';
+import { FaqItem } from './types';
 
-export const FAQ = () => {
+export const FAQ = async () => {
+  const faqs: FaqItem[] = await getFaq();
   return (
     <section className="bg-pinkSupperLight py-10 md:py-[60px] xl:py-[70px] ">
       <div className="container xl:w-[858px]">
         <h2 className="mb-6 md:mb-11 xl:text-center">{faq.title}</h2>
-        <Accordion />
+        <Accordion faqs={faqs} />
       </div>
     </section>
   );
--- src/sections/faq/types.ts
+++ src/sections/faq/types.ts
@@ -3,7 +3,3 @@ export type FaqItem = {
   question: string;
   answer: string;
 };
-
-export type  AccordionProps = {
-  faqs: FaqItem[];
-}
