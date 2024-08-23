d9f84469f516a065f9cf1621c38b0b1f38786cd5
Marynryn <murmar95@gmail.com>
2024-08-23 10:58:45 +0300

    img in div

--- src/sections/hero/hero.tsx
+++ src/sections/hero/hero.tsx
@@ -7,16 +7,20 @@ import content from '@/data/hero.json';
 export const Hero = () => {
   return (
     <section className="pt-11 pb-[45px] md:pb-[72px] md:pt-[90px] xl:pt-[109px] xl:pb-[70px]">
-      <div className="container smOnly:relative flex flex-col md:gap-20 xl:gap-[37px]">
-        <Image
-          src={content.image.src}
-          width={939}
-          height={450}
-          priority
-          alt={content.image.alt}
-          className=" smOnly:absolute h-[186px] left-0 right-0 mb-12 md:order-2 md:left-0 md:right-0 md:h-[362px] md:mb-0 xl:h-[450px] xl:w-[939px] xl:mx-auto"
-        />
-        <div className=" smOnly:mt-[234px] text-center md:px-[43px]  md:order-1  xl:px-[311px]">
+      <div className="container  flex flex-col md:gap-20 xl:gap-[37px]">
+        <div className="smOnly:-mx-5 md:order-2 ">
+          {' '}
+          <Image
+            src={content.image.src}
+            width={939}
+            height={450}
+            priority
+            alt={content.image.alt}
+            className="    md:left-0 md:right-0 md:h-[362px]  xl:h-[450px] xl:w-[939px] xl:mx-auto"
+          />
+        </div>
+
+        <div className="smOnly:mt-12  text-center md:px-[43px]  md:order-1  xl:px-[311px]">
           <h1 className="mb-6 text-4xl font-semibold leading-[0.9] font-geologica md:text-[70px] md:mb-4">
             {content.title}
           </h1>
