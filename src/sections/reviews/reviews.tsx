55c05f2b8b50f6a927b287ab40b3b1ddcf347f78
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-08-23 16:19:29 +0300

    img reviews

--- src/sections/reviews/reviews.tsx
+++ src/sections/reviews/reviews.tsx
@@ -9,45 +9,20 @@ export const Reviews = () => {
         <div className="flex flex-col xl:flex-row xl:items-center xl:justify-between">
           <ReviewsBlock />
 
-          <Image
-            src={reviews.image.path}
-            width={254}
-            height={237}
-            alt={reviews.image.alt}
-            className="smOnly:mx-auto smOnly:mt-4 w-[254px] h-[237px] object-cover 
-             md:w-[633px] md:h-[488px] mdOnly:mx-auto mdOnly:mt-[60px] 
-             xl:w-1/2 xl:ml-auto xl:w-[607px] xl:h-[574px] object-[left]"
-          />
+          <div className="smOnly:px-[33px] smOnly:mt-4 w-full mdOnly:mt-[60px]">
+            <Image
+              src={reviews.image.path}
+              width={770.5}
+              height={594}
+              alt={reviews.image.alt}
+              objectFit="cover"
+              className="object-cover w-fill h-auto
+                md:w-[633px] md:h-[488px] mdOnly:mx-auto 
+                xl:ml-auto xl:w-[607px] xl:h-[574px] xl:object-[left]"
+            />
+          </div>
         </div>
       </div>
     </section>
   );
 };
-
-// import { ReviewsBlock } from '@/components/common/reviews-bkock/reviews-block';
-// import reviews from '@/data/reviews.json';
-// import Image from 'next/image';
-
-// export const Reviews = () => {
-//   return (
-//     <section className="pt-[45px] md:pt-[50px] xl:pt-[70px]">
-//       <div className="container">
-//         <div className="flex flex-col xl:flex-row xl:items-center xl:justify-between">
-//           <ReviewsBlock />
-
-//           <div className='relative w-full  md:h-[488px] xl:h-[574px] overflow-hidden max-w-[480px] md:max-w-[633px] xl:max-w-[607px]'
-//           >
-//             <Image
-//               src={reviews.image.path}
-//               alt={reviews.image.alt}
-//               layout="fill"
-//               objectFit="cover"
-//               objectPosition="center"
-//               className="w-full h-auto  sm:object-center"
-//             />
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
