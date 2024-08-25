c5c386491459cac4d13c679844c0c260398d6d2e
bonny-art <svitlana.otenko@gmail.com>
2024-08-25 19:56:49 +0300

    Add metadata, favicon, ogp image

--- src/app/(studio)/admin/layout.tsx
+++ src/app/(studio)/admin/layout.tsx
@@ -6,6 +6,9 @@ import { Header } from '@/layout/Header/header';
 export const metadata: Metadata = {
   title: `${data.metadataAdmin.title}`,
   description: `${data.metadataAdmin.description}`,
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
