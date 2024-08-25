b932dc4a916262000bac8f8a064610d2526e4a6f
Marynryn <murmar95@gmail.com>
2024-08-25 15:36:53 +0300

    styles policy

--- src/app/(site)/policy/page.tsx
+++ src/app/(site)/policy/page.tsx
@@ -1,20 +1,13 @@
-'use client';
 import { PolicyData } from '@/sections/policy/types';
 import React, { useEffect } from 'react';
 import { getPolitics } from '../../../../sanity/api';
 import { Policy } from '@/sections/policy/policy';
 
-export default function PolicyPage() {
-  const [politics, setPolitics] = React.useState<PolicyData[]>([]);
-
-  useEffect(() => {
-    async function fetchPolitics() {
-      const data = await getPolitics();
-      setPolitics(data);
-    }
-
-    fetchPolitics();
-  }, []);
-
+const PolicyPage = async () => {
+  let politics = null;
+  try {
+    politics = await getPolitics();
+  } catch (err) {}
   return <main>{politics && <Policy politics={politics} />}</main>;
-}
+};
+export default PolicyPage;
