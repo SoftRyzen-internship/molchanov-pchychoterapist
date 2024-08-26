2348b5caa0baca10e62c5c89103e07cb8f24c169
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-08-26 18:03:26 +0300

    adding a link component fixed-footer

---
+++ src/components/common/footer-links/footer-links.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import Link from 'next/link';
+import GoItSvg from '@/../../public/assets/images/icons/go-it.svg';
+import SoftRyzenSvg from '@/../../public/assets/images/icons/softryzen.svg';
+import footer from '@/data/footer.json';
+import { LinkKey } from './types';
+
+const iconComponents = {
+  goIt: GoItSvg,
+  softRyzen: SoftRyzenSvg,
+};
+
+const links = footer.links.map(link => ({
+  ...link,
+  component: iconComponents[link.key as LinkKey],
+}));
+
+export const FooterLinks = () => {
+  return (
+    <div className="flex items-center gap-4 stroke-white fill-white text-white">
+      {links.map(({ href, ariaLabel, component: IconComponent, width, height }) => (
+        <Link
+          key={href}
+          href={href}
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+          aria-label={ariaLabel}
+          className="group"
+        >
+          <IconComponent
+            width={width}
+            height={height}
+            className="group-hover:fill-pinkLight group-hover:stroke-pinkLight group-focus:fill-pinkLight group-focus:stroke-pinkLight transition-colors duration-300"
+          />
+        </Link>
+      ))}
+    </div>
+  );
+};
