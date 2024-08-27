d7483fc5c61a1f7adea07b8ebc906bcdc210cea5
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-08-27 14:42:18 +0300

    change svg quotes change-svg

--- src/components/common/reviews-bkock/reviews-block.tsx
+++ src/components/common/reviews-bkock/reviews-block.tsx
@@ -2,17 +2,15 @@ import { FC } from 'react';
 import { Slider } from '@/components/ui/slider/slider';
 import reviews from '@/data/reviews.json';
 import { ReviewCard } from '@/components/common/review-card/review-card';
-import Image from 'next/image';
+import QuotesSvg from '@/../public/assets/images/icons/quotes-left.svg';
 
 export const ReviewsBlock: FC = () => {
   return (
     <div className="xl:w-[467px]">
-      <Image
-        src={reviews.quotes.path}
-        width={32}
-        height={30}
-        alt={reviews.quotes.alt}
-        className="mb-5 md:w-[70px] md:mb-8 xl:mb-[57px]"
+      <QuotesSvg
+        width={70}
+        height={67}
+        className="mb-5 w-8 h-[30px] md:w-[70px] md:h-[67px] md:mb-8 xl:mb-[57px]"
       />
       <Slider>
         {reviews.reviews.map((review) => (
