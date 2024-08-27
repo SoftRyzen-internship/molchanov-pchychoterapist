4c3442e23c3c75643efd821031e6608695655fa0
Aleksandr2905 <natalukha@ukr.net>
2024-08-27 13:36:24 +0300

    fix request telegram

--- src/telegram/telegram.ts
+++ src/telegram/telegram.ts
@@ -1,5 +1,5 @@
 export const sendToTelegram = async (message: string) => {
-  const response = await fetch('/api/sendMessage', {
+  const response = await fetch('/api', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -13,3 +13,26 @@ export const sendToTelegram = async (message: string) => {
 
   return response.json();
 };
+
+// export const sendToTelegram = async (message: string) => {
+//   const response = await fetch(
+//     `https://api.telegram.org/bot${process.env.NEXT_PUBLIC_TELEGRAM_TOKEN}/sendMessage`,
+//     {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         chat_id: process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID,
+//         text: message,
+//         parse_mode: 'HTML',
+//       }),
+//     }
+//   );
+
+//   if (!response.ok) {
+//     throw new Error('Could not send message');
+//   }
+
+//   return response.json();
+// };
