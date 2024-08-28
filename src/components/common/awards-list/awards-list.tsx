3efe005337b79514b50c8770a17816d6c7c63b2e
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-08-28 14:03:54 +0300

    tested fixed-about

--- src/components/common/awards-list/awards-list.tsx
+++ src/components/common/awards-list/awards-list.tsx
@@ -4,7 +4,7 @@ import aboutData from '@/data/about.json';
 
 export const AwardsList = () => {
   return (
-    <ul className="flex flex-col gap-6 md:flex-row md:flex-wrap md:justify-between md:gap-x-0 md:gap-y-7 xl:gap-y-8 xl:w-[610px]">
+    <ul className="flex flex-col gap-6 md:flex-row md:flex-wrap md:justify-between  md:gap-y-7 xl:gap-x-0 xl:gap-y-8 xl:w-[610px]">
       {aboutData.awards.map((award) => (
         <AwardItem key={award.id} item={award} />
       ))}
--- src/sections/about/about.tsx
+++ src/sections/about/about.tsx
@@ -6,24 +6,24 @@ export const About = () => {
   return (
     <section id="about" className="py-[45px] md:py-[50px] xl:py-[105px]">
       <div className="container xl:relative">
-        <h2 className="h2 mb-10 md:mb-[60px] xl:mb-5">{aboutData.title}</h2>
+        <h2 className="h2 mb-10 md:w-[607px] md:mb-[60px] xl:mb-5">{aboutData.title}</h2>
 
         <div className="relative smOnly:mb-[120px] mdOnly:mb-[130px] xl:absolute xl:top-0.5 xl:left-[754px]">
           <div className="relative w-full md:w-[575px] xl:w-[505px] h-[411px] md:h-[667px] xl:h-[586px] rounded-lg overflow-hidden">
             <Image
-              src="/assets/images/webp/about/my-photo.webp"
-              alt="Сергій Молчанов"
+              src={aboutData.images.mainImage.src}
+              alt={aboutData.images.mainImage.alt}
               fill
               sizes="(max-width: 360px) 320px, (max-width: 767px) 440px, (max-width: 1279px) 575px, 505px"
               className="h-full w-full object-cover object-left"
             />
           </div>
 
-          <div className="absolute -bottom-20 left-0 w-full xl:w-[505px] smOnly:max-w-[440px] pl-[72px] md:pl-[367px] xl:pl-[196px] pr-[21px] md:pr-0 xl:pr-[14px]">
+          <div className="absolute -bottom-20 left-0 w-full smOnly:max-w-[440px] pl-[72px] pr-[21px] md:pr-0 md:pl-[367px] xl:w-[505px] xl:pl-[196px] xl:pr-[14px] xl:bottom-[-156px]">
             <div className="relative w-full md:w-[339px] xl:w-[295px] h-[152px] md:h-[252px] xl:h-[209px] rounded-lg overflow-hidden">
               <Image
-                src="/assets/images/webp/about/therapy-meeting.webp"
-                alt="Сеанс терапії Сергія Молчанова з групою клієнтів"
+                src={aboutData.images.secondaryImage.src}
+                alt={aboutData.images.secondaryImage.alt}
                 fill
                 sizes="(max-width: 360px) 227px, (max-width: 767px) 347px, (max-width: 1279px) 339px, 295px"
                 className="object-cover object-center"
@@ -32,9 +32,9 @@ export const About = () => {
           </div>
         </div>
 
-        <div className="flex flex-col gap-4 xl:gap-5 md:w-[520px] xl:w-[470px] mb-10 md:mb-9 xl:mb-11">
+        <div className="flex flex-col gap-4 md:w-[520px] mb-10 md:mb-8 xl:gap-5 xl:w-[470px] xl:mb-11">
           {aboutData.text.map((text, index) => (
-            <p key={index} className="md:font-medium md:text-base">
+            <p key={index} className="md:font-medium md:text-[16px]">
               {text}
             </p>
           ))}
