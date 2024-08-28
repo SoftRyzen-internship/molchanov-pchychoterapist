b4346b41b31cffff63ca00f7f26c8f25bdc36555
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-08-28 15:23:40 +0300

    top padding desc section tested-services

--- src/sections/services/services.tsx
+++ src/sections/services/services.tsx
@@ -4,7 +4,10 @@ import servicesData from '@/data/services.json';
 
 export const Services = ({ services }: ServicesProps) => {
   return (
-    <section id="services" className="py-[45px] md:py-[50px] xl:pt-[70px] xl:pb-[75px]">
+    <section
+      id="services"
+      className="py-[45px] md:py-[50px] xl:pt-[70px] xl:pb-[75px]"
+    >
       <div className="container">
         <h2 className="h2 mb-8 md:mb-[60px]">{servicesData.title}</h2>
 
