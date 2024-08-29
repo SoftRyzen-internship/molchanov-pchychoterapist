fae5fd61ea714e086bc70a7f1927c962531e0931
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-08-29 11:24:18 +0300

    added link Spilno fixed-footer-link

--- src/components/common/copyright-text/copyright-text.tsx
+++ src/components/common/copyright-text/copyright-text.tsx
@@ -1,22 +1,7 @@
-import Link from 'next/link';
 import footer from '@/data/footer.json';
-
-const { year, link, text } = footer.copyright;
 
 export const CopyrightText = () => {
   return (
-    <p className="flex ">
-      {year}&nbsp;
-      <Link
-        href={link.href}
-        target="_blank"
-        rel="noopener noreferrer nofollow"
-        className="link-underline-animation"
-        aria-label={link.ariaLabel}
-      >
-        {link.text}
-      </Link>
-      . {text}
-    </p>
+    <p>{footer.copyright}</p>
   );
 };
